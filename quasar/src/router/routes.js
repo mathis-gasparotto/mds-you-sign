990d8945e9094ebdfa94550d1ce7761e8d68394b
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-09-19 15:58:10 +0200

    :sparkles: Add auth routing + login page

--- quasar/src/router/routes.js
+++ quasar/src/router/routes.js
@@ -1,21 +1,43 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/welcome",
+    component: () => import("layouts/WelcomeLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue'), name: 'home' },
-      { path: '/account', component: () => import('pages/AccountPage.vue'), name: 'account' },
-      { path: '/lessons/:id', component: () => import('pages/SingleLessonPage.vue'), name: 'singleLesson' }
+      {
+        path: "login",
+        name: "login",
+        component: () => import("pages/LoginPage.vue")
+      },
+    ],
+  },
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: () => import("pages/IndexPage.vue")
+      },
+      {
+        path: "account",
+        name: "account",
+        component: () => import("pages/AccountPage.vue")
+      },
+      {
+        path: "lessons/:id",
+        name: "singleLesson",
+        component: () => import("pages/SingleLessonPage.vue")
+      }
     ]
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
 ]
 
 export default routes

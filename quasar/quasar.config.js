6ec31652cd114efd304ece1f57a343e967a5c201
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2023-09-20 23:24:11 +0200

    :bug: Fix qrcore scanner done!

--- quasar/quasar.config.js
+++ quasar/quasar.config.js
@@ -27,7 +27,6 @@ module.exports = configure(function (ctx) {
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
     boot: [
       // 'capacitor',
-      'scanner',
     ],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css

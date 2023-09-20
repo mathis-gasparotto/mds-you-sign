a232e50916ebaafc2ed775403942ce2d8bf6aca4
NicksEdits <61987116+NicksEdits@users.noreply.github.com>
2023-09-20 16:37:02 +0200

    [UPDATE] teacher mid

--- laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -47,6 +47,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->invalidate();
         session()->forget('role');
+        session()->forget('id');
         $request->session()->regenerateToken();
 
         return redirect('/');

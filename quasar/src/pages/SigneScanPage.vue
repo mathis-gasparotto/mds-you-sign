ebfaeabaefa1c62913f384706fbb24e4fae965bf
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-09-20 16:04:04 +0200

    :bug: Try to fix qrcode reader

--- quasar/src/pages/SigneScanPage.vue
+++ quasar/src/pages/SigneScanPage.vue
@@ -1,17 +1,21 @@
 <template>
     <q-page class="flex">
-      <qrcode-stream class="qrcode-reader" @detect="onDetect"></qrcode-stream>
+      <!-- <qrcode-stream class="qrcode-reader" @detect="onDetect"></qrcode-stream> -->
     </q-page>
   </template>
 
   <script>
-  import { QrcodeStream } from 'vue-qrcode-reader'
+  // import { QrcodeStream } from 'vue-qrcode-reader'
+  import { BarcodeScanner } from '@capacitor-community/barcode-scanner'
   import { useRoute } from 'vue-router'
 
   export default {
     name: 'SigneScan',
-    components: {
-      QrcodeStream
+    // components: {
+    //   QrcodeStream
+    // },
+    created () {
+      this.startScan()
     },
     setup () {
       const route = useRoute()
@@ -24,6 +28,23 @@
       onDetect (content) {
         console.log('Decoded', content[0].rawValue)
         this.$router.push({ name: 'singleLesson', params: { id: this.route.params.id } })
+      },
+      async startScan() {
+        // Check camera permission
+        // This is just a simple example, check out the better checks below
+        await BarcodeScanner.checkPermission({ force: true })
+
+        // make background of WebView transparent
+        // note: if you are using ionic this might not be enough, check below
+        BarcodeScanner.hideBackground()
+
+        const result = await BarcodeScanner.startScan() // start scanning and wait for a result
+
+        // if the result has content
+        if (result.hasContent) {
+          console.log('Decoded', result.content)
+          this.$router.push({ name: 'singleLesson', params: { id: this.route.params.id } })
+        }
       }
     }
   }

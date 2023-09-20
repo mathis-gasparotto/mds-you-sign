90c5e3cf6b292d999ad50f04779c2d920758cad2
NicksEdits <61987116+NicksEdits@users.noreply.github.com>
2023-09-20 15:54:52 +0200

    [ADD] Lessons + rules teacher admins

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CoursesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,17 +19,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::prefix('/dashboard')->controller(CoursesController::class)->group(function () {
+    Route::get('/', 'show')->name('dashboard')->middleware(['auth','verified','teacher']);
+    Route::get('/course', 'showCourse')->middleware(['auth','verified','teacher'])->name('course');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
 Route::get('/register-account',function (){
     return view('register-account');
 })->middleware(['auth', 'verified'])->name('register-account');
 
 require __DIR__.'/auth.php';
+
+

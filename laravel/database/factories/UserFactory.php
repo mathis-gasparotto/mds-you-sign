12db4223c0f189e27e361a9d6367502e37fa9c0f
NicksEdits <61987116+NicksEdits@users.noreply.github.com>
2023-09-20 10:25:44 +0200

    [ADD] Seeder finition de la base de donné

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Classe;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -18,21 +20,24 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role' => 'student',
+            'password' =>  Hash::make('password'), // password
             'remember_token' => Str::random(10),
+            //'classe_id' =>  function () {return Classe::inRandomOrder()->first()->id;}
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): static
+    public function teacher(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            'role' => 'teacher',
         ]);
     }
 }

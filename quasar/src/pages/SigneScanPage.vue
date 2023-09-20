82a840ffd6f39220be3797cd91fa44823bce607c
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-09-20 10:37:59 +0200

    :sparkles: Add QRCode (in progress)

--- quasar/src/pages/SigneScanPage.vue
+++ quasar/src/pages/SigneScanPage.vue
@@ -1,11 +1,39 @@
 <template>
     <q-page class="flex page-container">
-      Signe & Scan
+      <qrcode-stream :camera="camera" @decode="onDecode"></qrcode-stream>
     </q-page>
   </template>
 
   <script>
+  import { QrcodeStream } from 'vue-qrcode-reader'
+
   export default {
-    name: 'SigneScan'
+    name: 'SigneScan',
+    components: {
+      QrcodeStream
+    },
+    data () {
+      return {
+        camera: 'auto',
+        showCamera: true,
+        result: ''
+      }
+    },
+    methods: {
+      async onDecode (content) {
+        this.result = content
+        console.log(content)
+        this.turnCameraOff()
+      },
+      turnCameraOn () {
+        this.camera = 'auto'
+        this.showCamera = true
+      },
+
+      turnCameraOff () {
+        this.camera = 'off'
+        this.showCamera = false
+      }
+    }
   }
   </script>

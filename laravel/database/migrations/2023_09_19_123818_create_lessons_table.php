c5e1b4dab6b1a78ad6234c8694d7c71e6b69c4ec
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-09-21 13:42:15 +0200

    fix(api): set engine InnoDB for all migrations

--- laravel/database/migrations/2023_09_19_123818_create_lessons_table.php
+++ laravel/database/migrations/2023_09_19_123818_create_lessons_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('lessons', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->id();
             $table->date('start_at');
             $table->date('end_at');

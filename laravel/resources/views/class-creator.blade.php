3b40b7858338fb3f9d42d4fa15eabddda947212c
jadegurnaud <jade.gurnaud@gmail.com>
2023-09-22 13:40:31 +0200

    visuel

--- laravel/resources/views/class-creator.blade.php
+++ laravel/resources/views/class-creator.blade.php
@@ -3,15 +3,15 @@
     <div class="w-full py-6 sm:px-12 px-3">
         <div class="text-center w-full pb-6">
         <button class="w-full">
-            <x-danger-button
+            <x-warning-button
                 x-data=""
                 class=" "
                 x-on:click.prevent="$dispatch('open-modal', 'confirm-class-create')"
             >
                 <p class="text-center">
-               Creer une classe
+               Créer une classe
                 </p>
-            </x-danger-button>
+            </x-warning-button>
             <x-modal name="confirm-class-create" :show="$errors->classeCreation->isNotEmpty()" focusable>
                 <div class="p-3">
                     <header>
@@ -84,7 +84,7 @@
                     </td>
                     <td class="text-center py-5">
                         <button>
-                            <x-danger-button
+                            <x-warning-button
                                 x-data=""
                                 x-on:click.prevent="$dispatch('open-modal', 'confirm-class-update-{{$classe->id}}')"
                             >
@@ -92,7 +92,7 @@
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                 </svg>
-                            </x-danger-button>
+                            </x-warning-button>
                             <x-modal name="confirm-class-update-{{$classe->id}}" :show="$showModalClasseUpdate" focusable>
                                <div class="p-3">
                                 <header>
